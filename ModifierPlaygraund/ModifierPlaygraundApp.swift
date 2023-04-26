f9c046214982d054a7e54d8c17dff099b0ee0957
Pavlo Snizhko <pavel.snizhko.2000@gmail.com>
2023-04-26 11:28:37 +0300

    Initial Commit

---
+++ ModifierPlaygraund/ModifierPlaygraundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifierPlaygraundApp.swift
+//  ModifierPlaygraund
+//
+//  Created by Павло Сніжко on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ModifierPlaygraundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
